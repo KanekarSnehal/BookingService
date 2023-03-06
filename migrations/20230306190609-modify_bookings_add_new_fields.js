1a928b01ed3587c9be20636f7e4355b86a80e6d1
KanekarSnehal <snehalkanekar43@gmail.com>
2023-03-07 00:45:52 +0530

    migrations to add new columns in Bookings table

---
+++ migrations/20230306190609-modify_bookings_add_new_fields.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     */
+    await queryInterface.addColumn('Bookings', 'noOfSeats', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    });
+
+    await queryInterface.addColumn('Bookings', 'totalCost', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Bookings', 'noOfSeats');
+    await queryInterface.removeColumn('Bookings', 'totalCost');
+  }
+};
